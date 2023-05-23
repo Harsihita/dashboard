fd99c2c528184bc3b997d125fcf74fb8d5de52b7
Harshita Gupta <harshitag1623@gmail.com>
2023-05-23 10:37:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// function myFunction(){
+//     var element = document.getElementById("btn")
+//     var line= document.getElementById("item")
+//     line.style.display="none";
+//     element.addEventListener("click",(event)=>{
+//         if(line.style.display=="none"){
+//             line.style.display="block"
+//         }else{
+//             line.style.display=="none"
+//         }
+//     })
+// }
